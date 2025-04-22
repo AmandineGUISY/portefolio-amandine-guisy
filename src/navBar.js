2e10fcd8db6e6b9d07036ebdaacd595348e8694a
AmandineGUISY <aguisy@yahoo.fr>
2025-04-22 17:00:06 +0200

    Main/ totale rework of the project

---
+++ src/navBar.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './navBar.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">Amandine</div>
+      <ul className="navbar-links">
+        <li><a href="/">Accueil</a></li>
+        <li><a href="/about">À propos</a></li>
+        <li><a href="/skills">Compétences</a></li>
+        <li><a href="/project">Projets</a></li>
+        <li><a href="/contact">Contact</a></li>
+      </ul>
+      <div className="navbar-icons">
+        <a href="https://github.com/ton-profil" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={faGithub} />
+        </a>
+        <a href="https://linkedin.com/in/ton-profil" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={faLinkedin} />
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
